26ad1ec747e35cfdc0db7e5d2ccedd83f63316f4
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2024-10-04 17:58:17 +0300

    Check if Draggable is loaded

--- src/main/resources/assets/partyboi.js
+++ src/main/resources/assets/partyboi.js
@@ -9,32 +9,34 @@ submitBtns.forEach(submitBtn => {
     })
 })
 
-const sortableSelector = '.sortable'
-const sortableContainers = document.querySelectorAll(sortableSelector)
+if (window.Draggable) {
+    const sortableSelector = '.sortable'
+    const sortableContainers = document.querySelectorAll(sortableSelector)
 
-sortableContainers.forEach(container => {
-    const sortable = new window.Draggable.Sortable(
-        container,
-        {
-            draggable: container.dataset.draggable,
-            handle: container.dataset.handle,
-            mirror: {
-                appendTo: container,
-                constrainDimensions: true
+    sortableContainers.forEach(container => {
+        const sortable = new window.Draggable.Sortable(
+            container,
+            {
+                draggable: container.dataset.draggable,
+                handle: container.dataset.handle,
+                mirror: {
+                    appendTo: container,
+                    constrainDimensions: true
+                }
             }
-        }
-    )
+        )
 
-    const ids = Array.from(container.querySelectorAll(container.dataset.draggable)).map(i => i.dataset.dragid)
+        const ids = Array.from(container.querySelectorAll(container.dataset.draggable)).map(i => i.dataset.dragid)
 
-    sortable.on('sortable:sorted', (event) => {
-        ids.splice(event.newIndex, 0, ...ids.splice(event.oldIndex, 1))
-        fetch(container.dataset.callback, {
-            method: 'PUT',
-            body: JSON.stringify(ids),
-            headers: {
-                'Content-Type': 'application/json'
-            }
+        sortable.on('sortable:sorted', (event) => {
+            ids.splice(event.newIndex, 0, ...ids.splice(event.oldIndex, 1))
+            fetch(container.dataset.callback, {
+                method: 'PUT',
+                body: JSON.stringify(ids),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
         })
     })
-})
+}
