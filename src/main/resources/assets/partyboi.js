ba6b24a219e55222d58dd0700d82ebb8290dfbb7
Ilkka Hänninen <ilkka.hanninen@gmail.com>
2024-11-01 12:11:54 +0200

    Fix broken interactive elements after page refresh

--- src/main/resources/assets/partyboi.js
+++ src/main/resources/assets/partyboi.js
@@ -1,5 +1,51 @@
 "use strict";
 
+function initInteractions(target) {
+  // Show a progress bar after click submit button
+  const submitBtns = target.querySelectorAll('input[type="submit"]');
+
+  submitBtns.forEach((submitBtn) => {
+    submitBtn.addEventListener("click", () => {
+      submitBtn.setAttribute("disabled", "disabled");
+      const progress = document.createElement("progress");
+      submitBtn.after(progress);
+      submitBtn.form.submit();
+    });
+  });
+
+  // Activate sortable lists
+  if (window.Draggable) {
+    const sortableSelector = ".sortable";
+    const sortableContainers = target.querySelectorAll(sortableSelector);
+
+    sortableContainers.forEach((container) => {
+      const sortable = new window.Draggable.Sortable(container, {
+        draggable: container.dataset.draggable,
+        handle: container.dataset.handle,
+        mirror: {
+          appendTo: container,
+          constrainDimensions: true,
+        },
+      });
+
+      const ids = Array.from(
+        container.querySelectorAll(container.dataset.draggable)
+      ).map((i) => i.dataset.dragid);
+
+      sortable.on("sortable:sorted", (event) => {
+        ids.splice(event.newIndex, 0, ...ids.splice(event.oldIndex, 1));
+        fetch(container.dataset.callback, {
+          method: "PUT",
+          body: JSON.stringify(ids),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+      });
+    });
+  }
+}
+
 // Smooth page reload
 async function smoothReload() {
   try {
@@ -12,11 +58,11 @@ async function smoothReload() {
     const reloadTarget = document.querySelector("#reload-section");
 
     if (reloadSection && reloadTarget) {
-      reloadTarget.innerHTML = reloadSection[1];
+      setContent(reloadTarget, reloadSection[1]);
     } else {
       const body = html.match(/.*(<body.*?>.*<\/body>).*/is);
       if (body) {
-        document.body.innerHTML = body[1];
+        setContent(document.body, body[1]);
       }
     }
     window.dispatchEvent(new Event("resize"));
@@ -25,46 +71,9 @@ async function smoothReload() {
   }
 }
 
-// Show a progress bar after click submit button
-const submitBtns = document.querySelectorAll('input[type="submit"]');
-
-submitBtns.forEach((submitBtn) => {
-  submitBtn.addEventListener("click", () => {
-    submitBtn.setAttribute("disabled", "disabled");
-    const progress = document.createElement("progress");
-    submitBtn.after(progress);
-    submitBtn.form.submit();
-  });
-});
-
-// Activate sortable lists
-if (window.Draggable) {
-  const sortableSelector = ".sortable";
-  const sortableContainers = document.querySelectorAll(sortableSelector);
-
-  sortableContainers.forEach((container) => {
-    const sortable = new window.Draggable.Sortable(container, {
-      draggable: container.dataset.draggable,
-      handle: container.dataset.handle,
-      mirror: {
-        appendTo: container,
-        constrainDimensions: true,
-      },
-    });
-
-    const ids = Array.from(
-      container.querySelectorAll(container.dataset.draggable)
-    ).map((i) => i.dataset.dragid);
-
-    sortable.on("sortable:sorted", (event) => {
-      ids.splice(event.newIndex, 0, ...ids.splice(event.oldIndex, 1));
-      fetch(container.dataset.callback, {
-        method: "PUT",
-        body: JSON.stringify(ids),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-    });
-  });
+function setContent(target, html) {
+  target.innerHTML = html;
+  initInteractions(target);
 }
+
+initInteractions(document);
