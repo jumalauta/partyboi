58718bd45d0b5cba18abd307a569f5732a26c899
Ilkka Hänninen <ilkka.hanninen@gmail.com>
2024-11-02 15:49:31 +0200

    Fix hidden "add compo" form if there was no compos in database

--- src/main/kotlin/party/jml/partyboi/admin/compos/AdminComposPage.kt
+++ src/main/kotlin/party/jml/partyboi/admin/compos/AdminComposPage.kt
@@ -21,75 +21,71 @@ object AdminComposPage {
         generalRules: Form<GeneralRules>,
     ) =
         Page("Compos") {
-            if (compos.isNotEmpty()) {
-                h1 { +"Compos" }
-
-                columns(if (compos.isNotEmpty()) {
-                    {
-                        article {
-                            table {
-                                thead {
-                                    tr {
-                                        th { +"Name" }
-                                        th { +"Entries" }
-                                        th {}
-                                    }
+            h1 { +"Compos" }
+            columns(if (compos.isNotEmpty()) {
+                {
+                    article {
+                        table {
+                            thead {
+                                tr {
+                                    th { +"Name" }
+                                    th { +"Entries" }
+                                    th {}
                                 }
-                                tbody {
-                                    compos.forEach { compo ->
-                                        tr {
-                                            td { a(href = "/admin/compos/${compo.id}") { +compo.name } }
-                                            td {
-                                                val count = entries.get(compo.id)?.size ?: 0
-                                                +count.toString()
-                                            }
-                                            td(classes = "align-right") {
-                                                toggleButton(
-                                                    compo.visible,
-                                                    IconSet.visibility,
-                                                    "/admin/compos/${compo.id}/setVisible"
-                                                )
-                                                toggleButton(
-                                                    compo.allowSubmit,
-                                                    IconSet.submitting,
-                                                    "/admin/compos/${compo.id}/setSubmit"
-                                                )
-                                                toggleButton(
-                                                    compo.allowVote,
-                                                    IconSet.voting,
-                                                    "/admin/compos/${compo.id}/setVoting"
-                                                )
-                                                toggleButton(
-                                                    compo.publicResults,
-                                                    IconSet.resultsPublic,
-                                                    "/admin/compos/${compo.id}/publishResults"
-                                                )
-                                            }
+                            }
+                            tbody {
+                                compos.forEach { compo ->
+                                    tr {
+                                        td { a(href = "/admin/compos/${compo.id}") { +compo.name } }
+                                        td {
+                                            val count = entries.get(compo.id)?.size ?: 0
+                                            +count.toString()
+                                        }
+                                        td(classes = "align-right") {
+                                            toggleButton(
+                                                compo.visible,
+                                                IconSet.visibility,
+                                                "/admin/compos/${compo.id}/setVisible"
+                                            )
+                                            toggleButton(
+                                                compo.allowSubmit,
+                                                IconSet.submitting,
+                                                "/admin/compos/${compo.id}/setSubmit"
+                                            )
+                                            toggleButton(
+                                                compo.allowVote,
+                                                IconSet.voting,
+                                                "/admin/compos/${compo.id}/setVoting"
+                                            )
+                                            toggleButton(
+                                                compo.publicResults,
+                                                IconSet.resultsPublic,
+                                                "/admin/compos/${compo.id}/publishResults"
+                                            )
                                         }
                                     }
                                 }
                             }
                         }
                     }
-                } else null,
-                    {
-                        dataForm("/admin/compos") {
-                            article {
-                                header { +"Add new compo" }
-                                fieldSet { renderFields(newCompoForm) }
-                                footer { submitInput { value = "Add" } }
-                            }
-                        }
+                }
+            } else null, {
+                dataForm("/admin/compos") {
+                    article {
+                        header { +"Add new compo" }
+                        fieldSet { renderFields(newCompoForm) }
+                        footer { submitInput { value = "Add" } }
+                    }
+                }
 
-                        article {
-                            dataForm("/admin/compos/generalRules") {
-                                fieldSet {
-                                    renderFields(generalRules)
-                                }
-                                footer { submitInput { value = "Save changes" } }
-                            }
+                article {
+                    dataForm("/admin/compos/generalRules") {
+                        fieldSet {
+                            renderFields(generalRules)
                         }
-                    })
-            }
+                        footer { submitInput { value = "Save changes" } }
+                    }
+                }
+            })
         }
 }
