6db29ccb263c129c7cb6cf90b01ef53be6540474
Ilkka Hänninen <ilkka.hanninen@gmail.com>
2025-02-16 18:44:45 +0200

    Add route configurations to default routing

--- src/main/kotlin/party/jml/partyboi/Application.kt
+++ src/main/kotlin/party/jml/partyboi/Application.kt
@@ -5,34 +5,16 @@ import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import party.jml.partyboi.assets.admin.configureAdminAssetsRouting
-import party.jml.partyboi.compos.admin.configureAdminComposRouting
-import party.jml.partyboi.schedule.admin.configureAdminScheduleRouting
-import party.jml.partyboi.screen.admin.configureAdminScreenRouting
-import party.jml.partyboi.settings.configureSettingsRouting
 import party.jml.partyboi.auth.configureAuthentication
-import party.jml.partyboi.auth.configureLoginRouting
-import party.jml.partyboi.compos.configureComposRouting
-import party.jml.partyboi.db.getDatabasePool
-import party.jml.partyboi.entries.configureEntriesRouting
-import party.jml.partyboi.plugins.configureDefaultRouting
-import party.jml.partyboi.plugins.configureHTTP
-import party.jml.partyboi.plugins.configureSerialization
-import party.jml.partyboi.assets.configureStaticContent
 import party.jml.partyboi.data.apiRespond
 import party.jml.partyboi.db.DbBasicMappers.asBoolean
 import party.jml.partyboi.db.Migrations
+import party.jml.partyboi.db.getDatabasePool
 import party.jml.partyboi.db.one
 import party.jml.partyboi.db.queryOf
-import party.jml.partyboi.frontpage.configureFrontPageRouting
-import party.jml.partyboi.qrcode.configureQrCodeRouting
-import party.jml.partyboi.replication.configureReplicationRouting
-import party.jml.partyboi.schedule.configureScheduleRouting
-import party.jml.partyboi.screen.configureScreenRouting
-import party.jml.partyboi.signals.configureSignalRouting
-import party.jml.partyboi.voting.configureVotingRouting
-import party.jml.partyboi.users.configureUserMgmtRouting
-import party.jml.partyboi.voting.admin.configureAdminVotingRouting
+import party.jml.partyboi.plugins.configureDefaultRouting
+import party.jml.partyboi.plugins.configureHTTP
+import party.jml.partyboi.plugins.configureSerialization
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
@@ -51,28 +33,7 @@ fun Application.module() {
     configureSerialization()
     configureHTTP()
     configureAuthentication(app)
-
-    configureDefaultRouting()
-    configureFrontPageRouting(app)
-    configureLoginRouting(app)
-    configureEntriesRouting(app)
-    configureComposRouting(app)
-    configureVotingRouting(app)
-    configureScreenRouting(app)
-    configureQrCodeRouting()
-    configureScheduleRouting(app)
-    configureAdminAssetsRouting(app)
-    configureSignalRouting(app)
-    configureSettingsRouting(app)
-
-    configureStaticContent()
-
-    configureAdminComposRouting(app)
-    configureAdminScreenRouting(app)
-    configureAdminScheduleRouting(app)
-    configureUserMgmtRouting(app)
-    configureAdminVotingRouting(app)
-    configureReplicationRouting(app)
+    configureDefaultRouting(app)
 
     launch {
         app.screen.start()
--- src/main/kotlin/party/jml/partyboi/plugins/Routing.kt
+++ src/main/kotlin/party/jml/partyboi/plugins/Routing.kt
@@ -4,11 +4,30 @@ import io.ktor.server.application.*
 import io.ktor.server.plugins.autohead.*
 import io.ktor.server.plugins.statuspages.*
 import kotlinx.html.h1
+import party.jml.partyboi.AppServices
+import party.jml.partyboi.assets.admin.configureAdminAssetsRouting
+import party.jml.partyboi.assets.configureStaticContent
+import party.jml.partyboi.auth.configureLoginRouting
+import party.jml.partyboi.compos.admin.configureAdminComposRouting
+import party.jml.partyboi.compos.configureComposRouting
+import party.jml.partyboi.entries.configureEntriesRouting
+import party.jml.partyboi.frontpage.configureFrontPageRouting
+import party.jml.partyboi.qrcode.configureQrCodeRouting
+import party.jml.partyboi.replication.configureReplicationRouting
+import party.jml.partyboi.schedule.admin.configureAdminScheduleRouting
+import party.jml.partyboi.schedule.configureScheduleRouting
+import party.jml.partyboi.screen.admin.configureAdminScreenRouting
+import party.jml.partyboi.screen.configureScreenRouting
+import party.jml.partyboi.settings.configureSettingsRouting
+import party.jml.partyboi.signals.configureSignalRouting
 import party.jml.partyboi.templates.Page
 import party.jml.partyboi.templates.components.errorMessage
 import party.jml.partyboi.templates.respondPage
+import party.jml.partyboi.users.configureUserMgmtRouting
+import party.jml.partyboi.voting.admin.configureAdminVotingRouting
+import party.jml.partyboi.voting.configureVotingRouting
 
-fun Application.configureDefaultRouting() {
+fun Application.configureDefaultRouting(app: AppServices) {
     install(StatusPages) {
         exception<Throwable> { call, cause ->
             call.respondPage(Page("Error") {
@@ -18,4 +37,25 @@ fun Application.configureDefaultRouting() {
         }
     }
     install(AutoHeadResponse)
+
+    configureFrontPageRouting(app)
+    configureLoginRouting(app)
+    configureEntriesRouting(app)
+    configureComposRouting(app)
+    configureVotingRouting(app)
+    configureScreenRouting(app)
+    configureQrCodeRouting()
+    configureScheduleRouting(app)
+    configureAdminAssetsRouting(app)
+    configureSignalRouting(app)
+    configureSettingsRouting(app)
+
+    configureStaticContent()
+
+    configureAdminComposRouting(app)
+    configureAdminScreenRouting(app)
+    configureAdminScheduleRouting(app)
+    configureUserMgmtRouting(app)
+    configureAdminVotingRouting(app)
+    configureReplicationRouting(app)
 }
