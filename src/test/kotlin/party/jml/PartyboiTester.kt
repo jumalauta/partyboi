33f8b86d9c4e0b7dbee0aa63155d893f3f5f4e8f
Ilkka Hänninen <ilkka.hanninen@gmail.com>
2025-02-18 19:40:43 +0200

    Refactor test base code

---
+++ src/test/kotlin/party/jml/PartyboiTester.kt
@@ -0,0 +1,63 @@
+package party.jml
+
+import io.ktor.client.*
+import io.ktor.client.plugins.cookies.*
+import io.ktor.client.request.*
+import io.ktor.client.request.forms.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.http.content.*
+import io.ktor.server.config.*
+import io.ktor.server.testing.*
+import it.skrape.core.htmlDocument
+import it.skrape.matchers.toBe
+import it.skrape.selects.Doc
+import kotlin.test.assertEquals
+
+interface PartyboiTester {
+    fun test(block: suspend ApplicationTestBuilder.(TestHtmlClient) -> Unit) {
+        testApplication {
+            environment {
+                config = ApplicationConfig("tests.yaml")
+            }
+            block(htmlClient())
+        }
+    }
+}
+
+fun ApplicationTestBuilder.htmlClient(): TestHtmlClient {
+    val client = createClient {
+        install(HttpCookies)
+    }
+    return TestHtmlClient(client)
+}
+
+class TestHtmlClient(val client: HttpClient) {
+    suspend fun get(
+        urlString: String,
+        expectedStatus: HttpStatusCode = HttpStatusCode.OK,
+        block: (Doc.() -> Unit)? = null
+    ) {
+        client.get(urlString).apply {
+            assertEquals(expectedStatus, status)
+            htmlDocument(bodyAsText()) {
+                if (block != null) block()
+            }
+        }
+    }
+
+    suspend fun post(urlString: String, formData: List<PartData>, block: Doc.(Headers) -> Unit) {
+        client.submitFormWithBinaryData(
+            url = urlString,
+            formData = formData
+        ).apply {
+            htmlDocument(bodyAsText()) {
+                block(headers)
+            }
+        }
+    }
+}
+
+fun Headers.redirectsTo(urlString: String) {
+    this["Location"].toBe(urlString)
+}
