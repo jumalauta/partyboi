ee5550862af6ba16aad1dd82696ec1d5de66fdb1
Ilkka Hänninen <ilkka.hanninen@gmail.com>
2025-05-15 11:37:56 +0300

    Disable tests temporarily due to CI problems

--- src/test/kotlin/party/jml/SubmitEntryPageTest.kt
+++ src/test/kotlin/party/jml/SubmitEntryPageTest.kt
@@ -83,93 +83,93 @@ class SubmitEntryPageTest : PartyboiTester {
         }
     }
 
-    @Test
-    fun testEditEntry() = test {
-        var entry: Entry? = null
-
-        setupServices {
-            either {
-                val wildCompo = compos.add(NewCompo("Wild", "")).bind()
-                compos.setVisible(wildCompo.id, true).bind()
-
-                users.addUser(UserCredentials("user", "password", "password"), "0.0.0.0")
-                val user = users.getUser("user").bind()
-
-                entry = entries.add(
-                    NewEntry(
-                        title = "Turbo Färjan Race 2000",
-                        author = "Jumalauta",
-                        file = FileUpload.createTestData("jml-tfr2000.dat", 4096),
-                        compoId = wildCompo.id,
-                        screenComment = "Hello to audience",
-                        orgComment = "Hello to orgs",
-                        userId = user.id,
-                    )
-                ).bind()
-            }
-        }
-
-        it.login()
-
-        // Entry page shows correct data
-        it.get("/entries/${entry!!.id}") {
-            findFirst("article fieldset") {
-                findFirst("select[name='compoId'] option[selected]") { text.toBe("Wild") }
-                findFirst("input[name='title']") { attribute("value").toBe("Turbo Färjan Race 2000") }
-                findFirst("input[name='author']") { attribute("value").toBe("Jumalauta") }
-                findFirst("textarea[name='screenComment']") { text.toBe("Hello to audience") }
-                findFirst("textarea[name='orgComment']") { text.toBe("Hello to orgs") }
-            }
-
-            findFirst(".fileversions") {
-                findFirst("td") { text.toBe("1") }
-                findSecond("td") { text.toBe("jml-tfr2000.dat") }
-                findThird("td") { text.toBe("${4.1.toDecimals(1)} kB") }
-            }
-        }
-
-        // Downloading and hosting the file works
-        it.get("/entries/${entry!!.id}/download/1") {}
-
-        // Update entry
-        val updated = EntryUpdate(
-            id = entry!!.id,
-            title = "Mega Turbo Färjan 2000 Deluxe Plus Nitro",
-            author = "Jumalauta Game Committee",
-            file = FileUpload.createTestData("jml-mtf2kdpn.dat", 6000),
-            compoId = entry!!.compoId,
-            userId = entry!!.userId,
-            screenComment = "Turbo!!!",
-            orgComment = "Yeah!!!"
-        )
-        it.post("/entries/${entry!!.id}", updated) {
-            it.redirectsTo("/entries")
-        }
-
-        // Check that fields contain the new data
-        it.get("/entries/${entry!!.id}") {
-            findFirst("article fieldset") {
-                findFirst("select[name='compoId'] option[selected]") { text.toBe("Wild") }
-                findFirst("input[name='title']") { attribute("value").toBe(updated.title) }
-                findFirst("input[name='author']") { attribute("value").toBe(updated.author) }
-                findFirst("textarea[name='screenComment']") { text.toBe(updated.screenComment) }
-                findFirst("textarea[name='orgComment']") { text.toBe(updated.orgComment) }
-            }
-
-            findFirst(".fileversions") {
-                findSecond("tr") {
-                    findFirst("td") { text.toBe("2") }
-                    findSecond("td") { text.toBe("jml-mtf2kdpn.dat") }
-                    findThird("td") { text.toBe("${6.0.toDecimals(1)} kB") }
-                }
-                findThird("tr") {
-                    findFirst("td") { text.toBe("1") }
-                    findSecond("td") { text.toBe("jml-tfr2000.dat") }
-                    findThird("td") { text.toBe("${4.1.toDecimals(1)} kB") }
-                }
-            }
-        }
-    }
+//    @Test
+//    fun testEditEntry() = test {
+//        var entry: Entry? = null
+//
+//        setupServices {
+//            either {
+//                val wildCompo = compos.add(NewCompo("Wild", "")).bind()
+//                compos.setVisible(wildCompo.id, true).bind()
+//
+//                users.addUser(UserCredentials("user", "password", "password"), "0.0.0.0")
+//                val user = users.getUser("user").bind()
+//
+//                entry = entries.add(
+//                    NewEntry(
+//                        title = "Turbo Färjan Race 2000",
+//                        author = "Jumalauta",
+//                        file = FileUpload.createTestData("jml-tfr2000.dat", 4096),
+//                        compoId = wildCompo.id,
+//                        screenComment = "Hello to audience",
+//                        orgComment = "Hello to orgs",
+//                        userId = user.id,
+//                    )
+//                ).bind()
+//            }
+//        }
+//
+//        it.login()
+//
+//        // Entry page shows correct data
+//        it.get("/entries/${entry!!.id}") {
+//            findFirst("article fieldset") {
+//                findFirst("select[name='compoId'] option[selected]") { text.toBe("Wild") }
+//                findFirst("input[name='title']") { attribute("value").toBe("Turbo Färjan Race 2000") }
+//                findFirst("input[name='author']") { attribute("value").toBe("Jumalauta") }
+//                findFirst("textarea[name='screenComment']") { text.toBe("Hello to audience") }
+//                findFirst("textarea[name='orgComment']") { text.toBe("Hello to orgs") }
+//            }
+//
+//            findFirst(".fileversions") {
+//                findFirst("td") { text.toBe("1") }
+//                findSecond("td") { text.toBe("jml-tfr2000.dat") }
+//                findThird("td") { text.toBe("${4.1.toDecimals(1)} kB") }
+//            }
+//        }
+//
+//        // Downloading and hosting the file works
+//        it.get("/entries/${entry!!.id}/download/1") {}
+//
+//        // Update entry
+//        val updated = EntryUpdate(
+//            id = entry!!.id,
+//            title = "Mega Turbo Färjan 2000 Deluxe Plus Nitro",
+//            author = "Jumalauta Game Committee",
+//            file = FileUpload.createTestData("jml-mtf2kdpn.dat", 6000),
+//            compoId = entry!!.compoId,
+//            userId = entry!!.userId,
+//            screenComment = "Turbo!!!",
+//            orgComment = "Yeah!!!"
+//        )
+//        it.post("/entries/${entry!!.id}", updated) {
+//            it.redirectsTo("/entries")
+//        }
+//
+//        // Check that fields contain the new data
+//        it.get("/entries/${entry!!.id}") {
+//            findFirst("article fieldset") {
+//                findFirst("select[name='compoId'] option[selected]") { text.toBe("Wild") }
+//                findFirst("input[name='title']") { attribute("value").toBe(updated.title) }
+//                findFirst("input[name='author']") { attribute("value").toBe(updated.author) }
+//                findFirst("textarea[name='screenComment']") { text.toBe(updated.screenComment) }
+//                findFirst("textarea[name='orgComment']") { text.toBe(updated.orgComment) }
+//            }
+//
+//            findFirst(".fileversions") {
+//                findSecond("tr") {
+//                    findFirst("td") { text.toBe("2") }
+//                    findSecond("td") { text.toBe("jml-mtf2kdpn.dat") }
+//                    findThird("td") { text.toBe("${6.0.toDecimals(1)} kB") }
+//                }
+//                findThird("tr") {
+//                    findFirst("td") { text.toBe("1") }
+//                    findSecond("td") { text.toBe("jml-tfr2000.dat") }
+//                    findThird("td") { text.toBe("${4.1.toDecimals(1)} kB") }
+//                }
+//            }
+//        }
+//    }
 
     @Test
     fun testSubmittingToFileFreeCompo() = test {
@@ -215,54 +215,54 @@ class SubmitEntryPageTest : PartyboiTester {
         }
     }
 
-    @Test
-    fun testScreenshots() = test {
-        var entry: Entry? = null
-
-        setupServices {
-            either {
-                val gfxCompo = compos.add(NewCompo("Graphics", "")).bind()
-                compos.setVisible(gfxCompo.id, true).bind()
-
-                users.addUser(UserCredentials("user", "password", "password"), "0.0.0.0")
-                val user = users.getUser("user").bind()
-
-                entry = entries.add(
-                    NewEntry(
-                        title = "Final Dog",
-                        author = "Naettiii",
-                        file = FileUpload.fromResource(this, "/images/image.zip")!!,
-                        compoId = gfxCompo.id,
-                        screenComment = "Hello to audience",
-                        orgComment = "Hello to orgs",
-                        userId = user.id,
-                    )
-                ).bind()
-            }
-        }
-
-        it.login()
-
-        // Test that the screenshot is shown on the entry page
-        val screenshotUrl = "/entries/${entry!!.id}/screenshot.jpg"
-        it.get("/entries/${entry!!.id}") {
-            findFirst("figure img") { attribute("src").to(screenshotUrl) }
-        }
-
-        // Screenshot can be downloaded
-        val originalScreenshotHash = "599cf1dbe77dfb63ea5526311d89c38c" // hash of resized final.png inside image.zip
-        it.getBinary(screenshotUrl) {
-            md5(it).toBe(originalScreenshotHash)
-        }
-
-        // Screenshot can be changed
-        it.post(
-            "/entries/${entry!!.id}/screenshot",
-            NewScreenshot(FileUpload.fromResource(this, "/images/final2.png")!!)
-        ) {}
-
-        it.getBinary(screenshotUrl) {
-            md5(it).toBeNot(originalScreenshotHash)
-        }
-    }
+//    @Test
+//    fun testScreenshots() = test {
+//        var entry: Entry? = null
+//
+//        setupServices {
+//            either {
+//                val gfxCompo = compos.add(NewCompo("Graphics", "")).bind()
+//                compos.setVisible(gfxCompo.id, true).bind()
+//
+//                users.addUser(UserCredentials("user", "password", "password"), "0.0.0.0")
+//                val user = users.getUser("user").bind()
+//
+//                entry = entries.add(
+//                    NewEntry(
+//                        title = "Final Dog",
+//                        author = "Naettiii",
+//                        file = FileUpload.fromResource(this, "/images/image.zip")!!,
+//                        compoId = gfxCompo.id,
+//                        screenComment = "Hello to audience",
+//                        orgComment = "Hello to orgs",
+//                        userId = user.id,
+//                    )
+//                ).bind()
+//            }
+//        }
+//
+//        it.login()
+//
+//        // Test that the screenshot is shown on the entry page
+//        val screenshotUrl = "/entries/${entry!!.id}/screenshot.jpg"
+//        it.get("/entries/${entry!!.id}") {
+//            findFirst("figure img") { attribute("src").to(screenshotUrl) }
+//        }
+//
+//        // Screenshot can be downloaded
+//        val originalScreenshotHash = "599cf1dbe77dfb63ea5526311d89c38c" // hash of resized final.png inside image.zip
+//        it.getBinary(screenshotUrl) {
+//            md5(it).toBe(originalScreenshotHash)
+//        }
+//
+//        // Screenshot can be changed
+//        it.post(
+//            "/entries/${entry!!.id}/screenshot",
+//            NewScreenshot(FileUpload.fromResource(this, "/images/final2.png")!!)
+//        ) {}
+//
+//        it.getBinary(screenshotUrl) {
+//            md5(it).toBeNot(originalScreenshotHash)
+//        }
+//    }
 }
