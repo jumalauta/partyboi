19b0e8abdd8a4447d68a4970a22a2518fe4c8dda
Ilkka Hänninen <ilkka.hanninen@gmail.com>
2025-05-23 14:05:33 +0300

    Refresh signal awaiting on page focus

--- src/main/resources/assets/partyboi.js
+++ src/main/resources/assets/partyboi.js
@@ -1,167 +1,185 @@
 "use strict";
 
 function initInteractions(target) {
-  // Show a progress bar after click submit button
-  const submitBtns = target.querySelectorAll('input[type="submit"]');
+    // Show a progress bar after click submit button
+    const submitBtns = target.querySelectorAll('input[type="submit"]');
 
-  submitBtns.forEach((submitBtn) => {
-    submitBtn.addEventListener("click", () => {
-      submitBtn.setAttribute("disabled", "disabled");
-      const progress = document.createElement("progress");
-      submitBtn.after(progress);
-      submitBtn.form.submit();
-    });
-  });
-
-  // Activate sortable lists
-  if (window.Draggable) {
-    const sortableSelector = ".sortable";
-    const sortableContainers = target.querySelectorAll(sortableSelector);
-
-    sortableContainers.forEach((container) => {
-      const sortable = new window.Draggable.Sortable(container, {
-        draggable: container.dataset.draggable,
-        handle: container.dataset.handle,
-        mirror: {
-          appendTo: container,
-          constrainDimensions: true,
-        },
-      });
-
-      const ids = Array.from(
-        container.querySelectorAll(container.dataset.draggable)
-      ).map((i) => i.dataset.dragid);
-
-      sortable.on("sortable:sorted", (event) => {
-        ids.splice(event.newIndex, 0, ...ids.splice(event.oldIndex, 1));
-        fetch(container.dataset.callback, {
-          method: "POST",
-          body: JSON.stringify(ids),
-          headers: {
-            "Content-Type": "application/json",
-          },
+    submitBtns.forEach((submitBtn) => {
+        submitBtn.addEventListener("click", () => {
+            submitBtn.setAttribute("disabled", "disabled");
+            const progress = document.createElement("progress");
+            submitBtn.after(progress);
+            submitBtn.form.submit();
         });
-      });
     });
-  }
 
-  // Mobile menu button
-  target.querySelectorAll(".mobile-nav-button").forEach((button) => {
-    button.onclick = () => {
-      document.body
-        .querySelector(".nav-and-content > aside")
-        .classList.toggle("open");
-    };
-  });
+    // Activate sortable lists
+    if (window.Draggable) {
+        const sortableSelector = ".sortable";
+        const sortableContainers = target.querySelectorAll(sortableSelector);
 
-  // Update file uploader according to the selected compo
-  const compoSelector = target.querySelector('select[name="compoId"]');
-  const fileUpload = target.querySelector('input[type="file"]');
-  if (compoSelector && fileUpload) {
-    async function updateFileUploadInput(selectedOptionElement) {
-      const optionData = selectedOptionElement?.dataset;
-      console.log("optionData", optionData);
-      fileUpload.parentElement.style.visibility =
-        !optionData || optionData.uploadenabled ? "visible" : "hidden";
-      if (optionData) {
-        fileUpload.setAttribute("accept", optionData.accept);
-      } else {
-        fileUpload.removeAttribute("accept");
-      }
+        sortableContainers.forEach((container) => {
+            const sortable = new window.Draggable.Sortable(container, {
+                draggable: container.dataset.draggable,
+                handle: container.dataset.handle,
+                mirror: {
+                    appendTo: container,
+                    constrainDimensions: true,
+                },
+            });
+
+            const ids = Array.from(
+                container.querySelectorAll(container.dataset.draggable)
+            ).map((i) => i.dataset.dragid);
+
+            sortable.on("sortable:sorted", (event) => {
+                ids.splice(event.newIndex, 0, ...ids.splice(event.oldIndex, 1));
+                fetch(container.dataset.callback, {
+                    method: "POST",
+                    body: JSON.stringify(ids),
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                });
+            });
+        });
     }
 
-    compoSelector.onchange = (event) => {
-      const value = event.target.value;
-      const option = event.target.querySelector(`option[value="${value}"]`);
-      updateFileUploadInput(option);
-    };
-    const defaultOption = compoSelector.querySelector("option:first-child");
-    updateFileUploadInput(defaultOption);
-  }
-
-  // Add Windows detection to specific links
-  if (window.navigator.oscpu?.includes("Windows")) {
-    const osLinks = document.querySelectorAll("a.osSpecific");
-    osLinks.forEach((link) => {
-      link.setAttribute("href", link.getAttribute("href") + "?win=true");
+    // Mobile menu button
+    target.querySelectorAll(".mobile-nav-button").forEach((button) => {
+        button.onclick = () => {
+            document.body
+                .querySelector(".nav-and-content > aside")
+                .classList.toggle("open");
+        };
     });
-  }
+
+    // Update file uploader according to the selected compo
+    const compoSelector = target.querySelector('select[name="compoId"]');
+    const fileUpload = target.querySelector('input[type="file"]');
+    if (compoSelector && fileUpload) {
+        async function updateFileUploadInput(selectedOptionElement) {
+            const optionData = selectedOptionElement?.dataset;
+            console.log("optionData", optionData);
+            fileUpload.parentElement.style.visibility =
+                !optionData || optionData.uploadenabled ? "visible" : "hidden";
+            if (optionData) {
+                fileUpload.setAttribute("accept", optionData.accept);
+            } else {
+                fileUpload.removeAttribute("accept");
+            }
+        }
+
+        compoSelector.onchange = (event) => {
+            const value = event.target.value;
+            const option = event.target.querySelector(`option[value="${value}"]`);
+            updateFileUploadInput(option);
+        };
+        const defaultOption = compoSelector.querySelector("option:first-child");
+        updateFileUploadInput(defaultOption);
+    }
+
+    // Add Windows detection to specific links
+    if (window.navigator.oscpu?.includes("Windows")) {
+        const osLinks = document.querySelectorAll("a.osSpecific");
+        osLinks.forEach((link) => {
+            link.setAttribute("href", link.getAttribute("href") + "?win=true");
+        });
+    }
 }
 
 // Smooth page reload
 async function smoothReload() {
-  try {
-    const response = await fetch(location);
-    const html = await response.text();
+    try {
+        const response = await fetch(location);
+        const html = await response.text();
 
-    const reloadSection = html.match(
-      /.*(<output id="reload-section">.*<\/output>).*/is
-    );
-    const reloadTarget = document.querySelector("#reload-section");
+        const reloadSection = html.match(
+            /.*(<output id="reload-section">.*<\/output>).*/is
+        );
+        const reloadTarget = document.querySelector("#reload-section");
 
-    if (reloadSection && reloadTarget) {
-      setContent(reloadTarget, reloadSection[1]);
-    } else {
-      const body = html.match(/.*(<body.*?>.*<\/body>).*/is);
-      if (body) {
-        setContent(document.body, body[1]);
-      }
+        if (reloadSection && reloadTarget) {
+            setContent(reloadTarget, reloadSection[1]);
+        } else {
+            const body = html.match(/.*(<body.*?>.*<\/body>).*/is);
+            if (body) {
+                setContent(document.body, body[1]);
+            }
+        }
+        window.dispatchEvent(new Event("resize"));
+    } catch (err) {
+        console.error(err);
     }
-    window.dispatchEvent(new Event("resize"));
-  } catch (err) {
-    console.error(err);
-  }
 }
 
 function setContent(target, html) {
-  target.innerHTML = html;
-  initInteractions(target);
+    target.innerHTML = html;
+    initInteractions(target);
 }
 
 // Resize screen monitoring
 function resizePreview() {
-  const container = document.querySelector(".screen-preview-container");
-  if (container) {
-    const frame = document.querySelector(".screen-preview");
-    const ratio = container.offsetWidth / frame.offsetWidth;
-    const scale = ratio * 100 + "%";
-    const width = frame.offsetWidth * ratio;
-    const height = frame.offsetHeight * ratio;
-    container.style.transform = "scale(" + scale + ")";
-    container.style.transformOrigin = "top left";
-    container.style.width = width + "px";
-    container.style.height = height + "px";
-  }
+    const container = document.querySelector(".screen-preview-container");
+    if (container) {
+        const frame = document.querySelector(".screen-preview");
+        const ratio = container.offsetWidth / frame.offsetWidth;
+        const scale = ratio * 100 + "%";
+        const width = frame.offsetWidth * ratio;
+        const height = frame.offsetHeight * ratio;
+        container.style.transform = "scale(" + scale + ")";
+        container.style.transformOrigin = "top left";
+        container.style.width = width + "px";
+        container.style.height = height + "px";
+    }
 }
 
 resizePreview();
 window.addEventListener("resize", resizePreview);
 
+const refreshOnSignalAbortControllers = {}
+
+function registerAbortController(key) {
+    const oldCtrl = refreshOnSignalAbortControllers[key];
+    if (oldCtrl) {
+        console.log("Refresh listener for", key)
+        oldCtrl.abort()
+    }
+    const ctrl = new AbortController()
+    refreshOnSignalAbortControllers[key] = ctrl;
+    return ctrl
+}
+
 // Update screen on signals
 function refreshOnSignal(signalType) {
-  let retryCount = 0;
+    const abortSignal = registerAbortController(signalType).signal;
+    let retryCount = 0;
 
-  function wait() {
-    if (++retryCount === 20) {
-      console.log("20 retries failed, reload page");
-      location.reload();
-      return;
+    function wait() {
+        if (++retryCount === 20) {
+            console.log("20 retries failed, reload page");
+            location.reload();
+            return;
+        }
+        console.log("Waiting for signal", signalType);
+        fetch(`/signals/${signalType}`, {signal: abortSignal})
+            .then(() => {
+                smoothReload();
+                wait();
+            })
+            .catch(() => {
+                const sleep = 1000 * retryCount;
+                console.log(`Fetch failed, retry in ${sleep} ms`);
+                setTimeout(wait, sleep);
+            });
     }
-    console.log("Waiting for signal", signalType);
-    fetch(`/signals/${signalType}`)
-      .then(() => {
-        smoothReload();
-        wait();
-      })
-      .catch(() => {
-        const sleep = 1000 * retryCount;
-        console.log(`Fetch failed, retry in ${sleep} ms`);
-        setTimeout(wait, sleep);
-      });
-  }
 
-  wait();
+    wait();
 }
+
+window.addEventListener("focus", () => {
+    Object.keys(refreshOnSignalAbortControllers).forEach(refreshOnSignal)
+});
 
 // Init
 initInteractions(document);
