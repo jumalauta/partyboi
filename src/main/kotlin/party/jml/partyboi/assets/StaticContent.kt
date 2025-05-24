a4de0aa280ae7430bcdfcce16ac4c1e39bf90ceb
Ilkka Hänninen <ilkka.hanninen@gmail.com>
2025-05-24 10:53:13 +0300

    Image slide fixes

--- src/main/kotlin/party/jml/partyboi/assets/StaticContent.kt
+++ src/main/kotlin/party/jml/partyboi/assets/StaticContent.kt
@@ -1,5 +1,6 @@
 package party.jml.partyboi.assets
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
 import io.ktor.server.routing.*
@@ -9,7 +10,17 @@ fun Application.configureStaticContent() {
     val uploadedAssetsDir = config().assetsDir.toFile()
     routing {
         staticFiles("/assets/uploaded", uploadedAssetsDir)
-        staticResources("/assets", "assets")
+        staticResources("/assets", "assets") {
+            cacheControl {
+                listOf(
+                    CacheControl.MaxAge(
+                        maxAgeSeconds = 31536000,
+                        proxyMaxAgeSeconds = 600,
+                        visibility = CacheControl.Visibility.Public
+                    )
+                )
+            }
+        }
         staticResources("/", "favicon")
     }
 }
