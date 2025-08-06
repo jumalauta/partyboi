6113f6127b677cbb2b2ed28125aa7f546d476939
Ilkka Hänninen <ilkka.hanninen@gmail.com>
2025-08-06 14:03:19 +0300

    Declare the Shadow plugin explicitly

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
     kotlin("jvm") version "2.0.20"
     id("io.ktor.plugin") version "2.3.12"
     id("org.jetbrains.kotlin.plugin.serialization") version "2.0.20"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "com.example"
