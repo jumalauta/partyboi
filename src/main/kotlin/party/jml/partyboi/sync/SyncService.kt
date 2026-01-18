8ec20afff249b694a080101ced9eb646fea9b23c
Ilkka Hänninen <ilkka.hanninen@gmail.com>
2026-01-18 13:08:12 +0200

    Add route to init instance sync immediately

--- src/main/kotlin/party/jml/partyboi/sync/SyncService.kt
+++ src/main/kotlin/party/jml/partyboi/sync/SyncService.kt
@@ -4,14 +4,17 @@ import arrow.core.raise.either
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.json.Json
 import party.jml.partyboi.AppServices
 import party.jml.partyboi.Service
+import party.jml.partyboi.data.InvalidConfiguration
 import party.jml.partyboi.system.AppResult
 import party.jml.partyboi.system.catchResult
-import java.net.URL
-import javax.ws.rs.client.Entity.json
+import java.net.URI
 
 enum class SyncedTable(val tableName: String) {
     Users("appuser"),
@@ -26,29 +29,41 @@ enum class SyncedTable(val tableName: String) {
     SlideSets("slideset"),
     Triggers("trigger"),
     Votes("vote"),
-    VoteKeys("votes_key")
+    VoteKeys("votekey")
 }
 
 class SyncService(app: AppServices) : Service(app) {
     private val db = DbSyncService(app)
-    private val apiKey = property<String?>("apiKey", "hunter2")
+    private val expectedApiKey = property<String?>("apiKey", null)
+    private val remoteInstance = property<RemoteInstance?>("remoteInstance", null)
+
     private val client = HttpClient(CIO) {
         expectSuccess = true
-        json {
-            Json {
-                ignoreUnknownKeys = false
-                isLenient = false
-            }
+        install(ContentNegotiation) {
+            json(
+                Json {
+                    ignoreUnknownKeys = false
+                    isLenient = false
+                }
+            )
         }
     }
 
     suspend fun isValidToken(token: String): AppResult<Boolean> =
-        apiKey.get().map { it == token }
+        expectedApiKey.get().map { it == token }
 
     suspend fun getTable(table: SyncedTable) = db.getTable(table.tableName)
     suspend fun putTable(table: Table) = db.putTable(table)
 
-    suspend fun downloadAndMergeTables(instance: RemoteInstance) = either {
+    suspend fun run() = either {
+        val instance = remoteInstance.get().bind()
+        if (instance == null) {
+            raise(InvalidConfiguration())
+        }
+        downloadAndMergeTables(instance).bind()
+    }
+
+    private suspend fun downloadAndMergeTables(instance: RemoteInstance) = either {
         SyncedTable.entries.forEach { table ->
             val importedData = downloadTable(instance, table).bind()
             db.putTable(importedData).bind()
@@ -57,7 +72,8 @@ class SyncService(app: AppServices) : Service(app) {
 
     suspend fun downloadTable(instance: RemoteInstance, table: SyncedTable): AppResult<Table> =
         catchResult {
-            client.get("${instance.url}/sync/table/${table.tableName}") {
+            client.get("${instance.address}/sync/table/${table.name.lowercase()}") {
+                accept(ContentType.Application.Json)
                 bearerAuth(instance.apiToken)
             }.body()
         }
@@ -82,7 +98,7 @@ class SyncService(app: AppServices) : Service(app) {
 }
 
 data class RemoteInstance(
-    val url: URL,
+    val address: URI,
     val apiToken: String,
 )
 
