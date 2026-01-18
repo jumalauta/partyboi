018cf31849ed6686bf64b5afb377b17331b2cccf
Ilkka Hänninen <ilkka.hanninen@gmail.com>
2026-01-18 15:19:42 +0200

    Download missing files

--- src/main/kotlin/party/jml/partyboi/sync/SyncRouting.kt
+++ src/main/kotlin/party/jml/partyboi/sync/SyncRouting.kt
@@ -1,7 +1,9 @@
 package party.jml.partyboi.sync
 
 import arrow.core.raise.either
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import party.jml.partyboi.AppServices
 import party.jml.partyboi.auth.adminRouting
@@ -9,12 +11,14 @@ import party.jml.partyboi.auth.syncRouting
 import party.jml.partyboi.auth.userSession
 import party.jml.partyboi.data.jsonRespond
 import party.jml.partyboi.data.parameterEnum
+import party.jml.partyboi.data.parameterUUID
 import party.jml.partyboi.data.processForm
 import party.jml.partyboi.form.Form
 import party.jml.partyboi.messages.MessageType
 import party.jml.partyboi.system.fullHost
 import party.jml.partyboi.templates.Redirection
 import party.jml.partyboi.templates.respondEither
+import party.jml.partyboi.templates.respondPage
 
 fun Application.configureSyncRouting(app: AppServices) {
     suspend fun renderPage(remoteForm: Form<RemoteInstance>? = null) = either {
@@ -77,6 +81,25 @@ fun Application.configureSyncRouting(app: AppServices) {
                 val name = call.parameterEnum<SyncedTable>("name").bind()
                 app.sync.getTable(name).bind()
             }
+        }
+
+        get("/sync/file/{fileId}") {
+            either {
+                val fileId = call.parameterUUID("fileId").bind()
+                app.files.getById(fileId).bind()
+            }.fold(
+                { call.respondPage(it) },
+                {
+                    call.response.header(
+                        HttpHeaders.ContentDisposition,
+                        ContentDisposition.Attachment.withParameter(
+                            ContentDisposition.Parameters.FileName,
+                            it.originalFilename
+                        ).toString()
+                    )
+                    call.respondFile(it.getStorageFile())
+                }
+            )
         }
     }
 }
